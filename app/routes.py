250ce2679ae94ffdbd1ea0c0dba571e8d8c38551
Jad <zeineddine.jad@gmail.com>
2019-12-20 00:29:07 -0600

    attempting to fix these shitty routes

--- app/routes.py
+++ app/routes.py
@@ -4,8 +4,8 @@ from pathlib import Path
 import numpy as np
 import json
 
-frames = []
-labels = []
+frames = np.asarray([])
+labels = np.asarray([])
 if(Path('frames.npy').is_file()):
     frames = np.load('frames.npy', allow_pickle=True)
     labels = np.load('labels.npy', allow_pickle=True)
@@ -19,18 +19,26 @@ def main():
 def index(): 
     data = json.loads(request.data)
     print(data['label'])
-    frames.append(data['frames'])
-    labels.append(data['label'])
+    global frames
+    global labels
+    frames = np.append(frames, data['frames'])
+    labels = np.append(labels, data['label'])
+    print("frames shape ", frames.shape)
+    print("labels shape ", labels.shape)
     return '0'
 
 @app.route('/save', methods=['GET'])
 def saver():
+    global frames
+    global labels
     np.save('frames', frames)
     np.save('labels', labels)
     return '0'
 
 @app.route('/delete', methods=['DELETE'])
 def delete():
+    global frames
+    global labels
     data = json.loads(request.data)
     index = data['index']
     del frames[index]
@@ -39,6 +47,8 @@ def delete():
 
 @app.route('/clear', methods=['GET'])
 def clear():
-    frames=[]
-    labels=[]
+    global frames
+    frames =np.asarray([])
+    global labels
+    labels =np.asarray([])
     return '0'
--- client/sketch.js
+++ client/sketch.js
@@ -12,54 +12,62 @@ let video;
 let poseNet;
 let poses = [];
 //Save the current models 
-function save() {
-  fetch('http://localhost:5000/save').then(function(){
-    console.log("saved")
-  }).catch(function(){
-    console.log("failed to save")
-  });
+function saveFrames() {
+fetch('http://localhost:5000/save',{
+  method:'GET',
+  body:{},
+  headers: {'Content-Type': 'text/plain'}
+}).then(function(){
+console.log("saved")
+}).catch(function(){
+console.log("failed to save")
+});
 }
 
 function clear() {
-  fetch('http://localhost:5000/clear').then(function(){
-    console.log("failed to clear")
-  }).catch(function(){
-    console.log("failed to clear")
-  });
+fetch('http://localhost:5000/clear', {
+  method:'GET',
+  body:{},
+  headers: {'Content-Type': 'text/plain'}
+}).then(function(){
+console.log("successfull clear")
+}).catch(function(){
+console.log("failed to clear")
+});
 }
 
 function setup() {
-  createCanvas(640, 480);
-  video = createCapture(VIDEO);
-  video.size(width, height);
-  // Create a new poseNet method with a single detection
-  poseNet = ml5.poseNet(video, modelReady);
-  // This sets up an event that fills the global variable "poses"
-  // with an array every time new poses are detected
-  poseNet.on('pose', function(results) {
-    poses = results;
-  });
-  // Hide the video element, and just show the canvas
-  video.hide();
+createCanvas(640, 480);
+video = createCapture(VIDEO);
+video.size(width, height);
+// Create a new poseNet method with a single detection
+poseNet = ml5.poseNet(video, modelReady);
+// This sets up an event that fills the global variable "poses"
+// with an array every time new poses are detected
+poseNet.on('pose', function(results) {
+poses = results;
+});
+// Hide the video element, and just show the canvas
+video.hide();
 }
 
 //Hook to delete a specific training point
 function deleteIndex() {
-  var index = document.getElementById('index').value;
-  fetch('http://localhost:5000/delete', {
-    'methods':'DELETE',
-    'data': {index:index},
-    'Content-Type':'text/plain'
-  }).then(function(){
-    console.log("Success")
-  }).catch(function(){
-    console.log("failure")
-  })
+var index = document.getElementById('index').value;
+fetch('http://localhost:5000/delete', {
+method:'DELETE',
+data: {index:index},
+headers: {'Content-Type': 'text/plain'}
+}).then(function(){
+console.log("Success")
+}).catch(function(){
+console.log("failure")
+})
 }
 
 //Hook to change status text when model loaded
 function modelReady() {
-  select('#status').html('Model Loaded');
+select('#status').html('Model Loaded');
 }
 
 var capture = [];
@@ -68,92 +76,92 @@ var date = 0
 var startcapturing = false
 //Hook for capturing the good label data
 function startcapturinggood() {
-  startcapturing = true
-  label = 0
+startcapturing = true
+label = 0
 }
 //Hook for capturing the bad label data
 function startcapturingbad(){
-  startcapturing = true
-  label = 1
+startcapturing = true
+label = 1
 }
 
 //Draws the points on the figure
 function draw() {  
-  image(video, 0, 0, width, height);
-  console.log(startcapturing)
-  //Keep updating the timer
-  if(startcapturing == false){
-    capture = []
-    date = Date.now()
-    var elapsed = 0
-    
+image(video, 0, 0, width, height);
+console.log(startcapturing)
+//Keep updating the timer
+if(startcapturing == false){
+capture = []
+date = Date.now()
+var elapsed = 0
+
+}
+//If there is hook to start capturing, start calculating the elapsed time
+if (startcapturing == true){
+elapsed = Date.now()
+capture.push(poses)
+let seconds = 5
+console.log(elapsed + " " + date)
+//if time is elapsed, send the data and label to the server
+if(elapsed > date + (seconds * 1000)){
+  console.log("capture", capture)
+  startcapturing = false
+  var data ={
+    frames: capture,
+    label: label
+  };
+  //POST request to add a training data
+  const response = fetch('http://localhost:5000/data', 
+  {
+    method: 'POST', 
+    body: JSON.stringify(data), 
+    headers: {'Content-Type': 'text/plain'}
   }
-  //If there is hook to start capturing, start calculating the elapsed time
-  if (startcapturing == true){
-    elapsed = Date.now()
-    capture.push(poses)
-    let seconds = 5
-    console.log(elapsed + " " + date)
-    //if time is elapsed, send the data and label to the server
-    if(elapsed > date + (seconds * 1000)){
-      console.log("capture", capture)
-      startcapturing = false
-      var data ={
-        frames: capture,
-        label: label
-      };
-      //POST request to add a training data
-      const response = fetch('http://localhost:5000/data', 
-      {
-        method: 'POST', 
-        body: JSON.stringify(data), 
-        headers: {'Content-Type': 'text/plain'}
-      }
-      ).then(function(df){
-        console.log(df)
-        console.log('Success:', JSON.stringify(json))
-        var json =  response.json();
-      })
-      .catch(function(){
-        console.log('fail')
-      })
-      }
+  ).then(function(df){
+    console.log(df)
+    console.log('Success:', JSON.stringify(json))
+    var json =  response.json();
+  })
+  .catch(function(){
+    console.log('fail')
+  })
   }
-  // We can call both functions to draw all keypoints and the skeletons
-  drawKeypoints();
-  drawSkeleton();
+}
+// We can call both functions to draw all keypoints and the skeletons
+drawKeypoints();
+drawSkeleton();
 }
 
 // A function to draw ellipses over the detected keypoints
 function drawKeypoints()  {
-  // Loop through all the poses detected
-  for (let i = 0; i < poses.length; i++) {
-    // For each pose detected, loop through all the keypoints
-    let pose = poses[i].pose;
-    for (let j = 0; j < pose.keypoints.length; j++) {
-      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
-      let keypoint = pose.keypoints[j];
-      // Only draw an ellipse is the pose probability is bigger than 0.2
-      if (keypoint.score > 0.2) {
-        fill(255, 0, 0);
-        noStroke();
-        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
-      }
-    }
+// Loop through all the poses detected
+for (let i = 0; i < poses.length; i++) {
+// For each pose detected, loop through all the keypoints
+let pose = poses[i].pose;
+for (let j = 0; j < pose.keypoints.length; j++) {
+  // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+  let keypoint = pose.keypoints[j];
+  // Only draw an ellipse is the pose probability is bigger than 0.2
+  if (keypoint.score > 0.2) {
+    fill(255, 0, 0);
+    noStroke();
+    ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
   }
+}
+}
 }
 
 // A function to draw the skeletons
 function drawSkeleton() {
-  // Loop through all the skeletons detected
-  for (let i = 0; i < poses.length; i++) {
-    let skeleton = poses[i].skeleton;
-    // For every skeleton, loop through all body connections
-    for (let j = 0; j < skeleton.length; j++) {
-      let partA = skeleton[j][0];
-      let partB = skeleton[j][1];
-      stroke(255, 0, 0);
-      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-    }
-  }
+// Loop through all the skeletons detected
+for (let i = 0; i < poses.length; i++) {
+let skeleton = poses[i].skeleton;
+// For every skeleton, loop through all body connections
+for (let j = 0; j < skeleton.length; j++) {
+  let partA = skeleton[j][0];
+  let partB = skeleton[j][1];
+  stroke(255, 0, 0);
+  line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+}
+}
 }
